f9ef43a6b9199e02cb9ed7f262b3671c5c76c903
Natalie Schulz <natalie.schulz@me.com>
2019-10-06 15:53:49 -0700

    Added flow, prettier, eslint, husky.

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
+// @flow
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+const App = (): any => (
+  <View>
+    <View>
+      <Text>Hello</Text>
     </View>
-  );
-}
+    <View>
+      <Text>Hello 2</Text>
+    </View>
+  </View>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;

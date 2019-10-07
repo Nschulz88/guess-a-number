350a3f7d7472872c6a6eb9170bf579df8088c422
Natalie Schulz <35382212+Nschulz88@users.noreply.github.com>
2019-10-06 23:08:05 -0700

    First component (#2)
    * Added flow-typed.
    
    * Adjusted main App Component to add List Item

--- App.js
+++ App.js
@@ -1,17 +1,63 @@
 // @flow
 
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
-const App = (): any => (
-  <View>
-    <View>
-      <Text>Hello</Text>
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textInput: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+});
+
+const App = (): any => {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputhandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  };
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={goalInputhandler}
+          placeholder="Course Goal"
+          style={styles.textInput}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <View>
+        {courseGoals.map(goal => (
+          <View key={goal} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </View>
     </View>
-    <View>
-      <Text>Hello 2</Text>
-    </View>
-  </View>
-);
+  );
+};
 
 export default App;

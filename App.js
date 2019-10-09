91d31f85d975742906050ae1142533ac0e02b467
Natalie Schulz <natalie.schulz@me.com>
2019-10-09 10:04:40 -0700

    Repo structure setup.

--- App.js
+++ App.js
@@ -1,51 +1,13 @@
 // @flow
 
-import React, { useState } from 'react';
-import { StyleSheet, View, FlatList, Button } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
 
-import GoalItem from './components/GoalItem.react';
-import GoalInput from './components/GoalInput.react';
-
-const styles = StyleSheet.create({
-  screen: {
-    padding: 50,
-  },
-});
 // $FlowFixMe
 const App = () => {
-  const [courseGoals, setCourseGoals] = useState([]);
-  const [modalIsOpen, setModalState] = useState(false);
-
-  const addGoalHandler = enteredGoal => {
-    if (enteredGoal.length === 0) {
-      return;
-    }
-    setCourseGoals(currentGoals => [
-      ...currentGoals,
-      { key: Math.random().toString(), value: enteredGoal },
-    ]);
-    setModalState(false);
-  };
-
-  const deleteGoalHandler = goalId => {
-    setCourseGoals(currentGoals =>
-      currentGoals.filter(item => item.key !== goalId),
-    );
-  };
   return (
-    <View style={styles.screen}>
-      <Button title="Add new Goal" onPress={() => setModalState(true)} />
-      <GoalInput
-        modalIsOpen={modalIsOpen}
-        onAddGoal={addGoalHandler}
-        isModalOpen={setModalState}
-      />
-      <FlatList
-        data={courseGoals}
-        renderItem={itemData => (
-          <GoalItem onDelete={deleteGoalHandler} item={itemData.item} />
-        )}
-      />
+    <View>
+      <Text>test</Text>
     </View>
   );
 };
